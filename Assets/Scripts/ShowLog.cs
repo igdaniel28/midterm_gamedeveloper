1f9e08fd17df05cc6c379ca5e1c7d705b8e47820
igdaniel28 <hphuc281225@gmail.com>
2026-01-19 00:09:01 +0700

    Initial commit - Unity project

---
+++ Assets/Scripts/ShowLog.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class ShowLog : MonoBehaviour
+{
+    void Start()
+    {
+        Debug.Log("Start called");
+    }
+
+    void Update()
+    {
+        Debug.Log("Update called");
+    }
+}
